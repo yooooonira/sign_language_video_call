038338d8d1c061516b0c6e37bf37f0b7f4a83663
oscar2272 <oscar2272@naver.com>
2025-08-19 16:27:52 +0900

    fix: friend serializers

--- drf_api/friend/serializers.py
+++ drf_api/friend/serializers.py
@@ -18,12 +18,12 @@ class UserSimpleSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['user_id', 'email', 'profile']
-    
+
     def get_profile(self, obj):
         # related_name이 'profile'이 아닐 수도 있으니 getattr로 안정 접근
         p = getattr(obj, 'profile', None)
         if p:
-            return ProfileSerializer(p).data
+            return ProfileSerializer(p, context=self.context).data
         nickname = (obj.email.split("@")[0] if obj.email else "")
         return {
             "nickname": nickname,
@@ -40,11 +40,11 @@ class FriendListSerializer(serializers.ModelSerializer): # 친구 목록 조회 
         fields = ['id', 'email', 'profile', 'cnt']
 
 
-      
 
-class FriendDetailSerializer(FriendListSerializer): # 친구 프로필 조회 
+
+class FriendDetailSerializer(FriendListSerializer): # 친구 프로필 조회
     profile = ProfileSerializer(read_only=True)
-    created_at = serializers.DateTimeField(read_only=True) 
+    created_at = serializers.DateTimeField(read_only=True)
     class Meta:
         model = User
         fields = ["id", "email", "profile","created_at"]
@@ -66,7 +66,7 @@ class SentRequestSerializer(serializers.ModelSerializer): #친추 보낸 목록
         fields = ["id", "to_user", "status"]
 
 class FriendRequestCreateSerializer(serializers.ModelSerializer): #친구 추가
-    to_user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())   #to_user:8 --> 8user에서 친구추가 
+    to_user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())   #to_user:8 --> 8user에서 친구추가
 
 
     class Meta:
@@ -81,7 +81,7 @@ class FriendRequestCreateSerializer(serializers.ModelSerializer): #친구 추가
             to_user=validated_data['to_user'],
             status='PENDING'
         )
-    
+
 
 class FriendRequestDetailSerializer(serializers.ModelSerializer): #친구 추가 (응답용)
     from_user = UserSimpleSerializer(read_only=True)
@@ -89,4 +89,4 @@ class FriendRequestDetailSerializer(serializers.ModelSerializer): #친구 추가
 
     class Meta:
         model = FriendRelations
-        fields = ["id", "from_user", "to_user", "status"] 
+        fields = ["id", "from_user", "to_user", "status"]
