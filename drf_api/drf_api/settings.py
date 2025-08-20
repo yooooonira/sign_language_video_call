83d6ce7d3e7523ce55bdd7bce358e696997cac5f
oscar2272 <oscar2272@naver.com>
2025-08-20 10:09:52 +0900

    test

--- drf_api/drf_api/settings.py
+++ drf_api/drf_api/settings.py
@@ -37,12 +37,12 @@ ALLOWED_HOSTS.extend(
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
 if FRONTEND_BASE_URL:
     CORS_ALLOWED_ORIGINS.append(FRONTEND_BASE_URL)
-    CSRF_TRUSTED_ORIGINS.append(BACKEND_BASE_URL)
+    #CSRF_TRUSTED_ORIGINS.append(BACKEND_BASE_URL)
 
 INSTALLED_APPS = [
     "core",
