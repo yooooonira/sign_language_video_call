2468d37641614d6bfad0a2d86e475c7952f9ab5b
oscar2272 <oscar2272@naver.com>
2025-08-22 14:24:44 +0900

    test

--- drf_api/call/routing.py
+++ drf_api/call/routing.py
@@ -1,10 +1,10 @@
 from django.urls import re_path
-from consumer import CallConsumer, CallNotifyConsumer
+from . import consumer
 
 websocket_urlpatterns = [
     # 기존 CallConsumer
-    re_path(r"ws/call/(?P<room_id>\w+)/$", CallConsumer.as_asgi()),
+    re_path(r"ws/call/(?P<room_id>\w+)/$", consumer.CallConsumer.as_asgi()),
 
     # 새 알림용 Consumer
-    re_path(r"ws/call-notify/$", CallNotifyConsumer.as_asgi()),
+    re_path(r"ws/call-notify/$", consumer.CallNotifyConsumer.as_asgi()),
 ]
