d2902bb36e4a0545c5ba285e5a39a839dd645fc5
oscar2272 <oscar2272@naver.com>
2025-08-24 13:41:59 +0900

    run.sh debug

--- drf_api/scripts/run.sh
+++ drf_api/scripts/run.sh
@@ -12,5 +12,7 @@ if [ "$ENV" = "production" ]; then
         --workers 4 \
         --log-level info
 else
-    uvicorn drf_api.asgi:application --host 0.0.0.0 --port 8000 --reload
+    # uvicorn drf_api.asgi:application --host 0.0.0.0 --port 8000 --reload
+    uvicorn drf_api.asgi:application --host 0.0.0.0 --port 8000 --reload --log-level debug
+
 fi
