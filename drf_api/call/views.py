cdeba0bd920943ebc836db4039f4cefbf4341cfb
oscar2272 <oscar2272@naver.com>
2025-08-27 14:39:07 +0900

    fix callMissedView

--- drf_api/call/views.py
+++ drf_api/call/views.py
@@ -126,8 +126,11 @@ class CallMissedView(APIView):
     통화가 걸렸지만 30초 동안 수락하지 않아 부재중 처리
     """
     def post(self, request):
+        print("CallMissedView 호출됨")
         room_id = request.data.get("room_id")
+        print(f"room_id: {room_id}")
         caller_id = request.data.get("receiver_id")
+        print(f"caller_id: {caller_id}")
         receiver = request.user
 
         caller = get_object_or_404(User, id=caller_id)
