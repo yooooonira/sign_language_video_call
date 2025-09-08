5c203dafbea8b82eb9e1e94ec0e87b84ec68e0a7
oscar2272 <oscar2272@naver.com>
2025-09-08 10:07:25 +0900

    test log

--- fastapi_api/app/websocketServer.py
+++ fastapi_api/app/websocketServer.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Query
-from .state import hub  #허브에 등록용 
+from .state import hub  #허브에 등록용
 import asyncio, json, logging
 
 logger = logging.getLogger(__name__)
@@ -14,17 +14,17 @@ async def websocket_endpoint(  # 프런트에서 값가져오기
 ):
     await websocket.accept() # 클라이언트의 WebSocket 연결 요청을 수락
     await hub.add(websocket, role=role, room=room) # 허브에 등록
-    logger.info("연결됨 role=%s room=%s", role, room or "(없음)")
-
+    logger.info("연결됨 logger role=%s room=%s", role, room or "(없음)")
+    print("웹소켓 연결됨 print", role, room)
     try:
         while True:
-            message = await websocket.receive() #프런트에서 받고 
+            message = await websocket.receive() #프런트에서 받고
 
             if message.get("type") == "websocket.disconnect":#못받으면 break 나가기
                 break
 
-            if "text" in message and message["text"] is not None: #받은거 처리 
-                raw = message["text"] #원천 
+            if "text" in message and message["text"] is not None: #받은거 처리
+                raw = message["text"] #원천
                 logger.info("프런트에서 수신(raw): %s", raw[:200])
 
                 # JSON이면 type 기반 라우팅
@@ -46,7 +46,7 @@ async def websocket_endpoint(  # 프런트에서 값가져오기
                 if mtype == "hand_landmarks":
                     try:
                         from app import main  # 순환 import 방지: 런타임에 불러오기
-                        pred_idx, probs = main.predict_landmarks(data.get("data")) #추론 
+                        pred_idx, probs = main.predict_landmarks(data.get("data")) #추론
 
                         # 클라이언트에 ai_result 전달
                         result = {
