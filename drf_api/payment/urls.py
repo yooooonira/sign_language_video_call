e2933b9fc4e65e00b0a736542a5918267af37ff8
oscar2272 <oscar2272@naver.com>
2025-09-09 14:39:18 +0900

    git merge (toss webhook,docker-compose.prod.yml, CI/CD)

--- drf_api/payment/urls.py
+++ drf_api/payment/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("create/", views.PaymentPrepareView.as_view(), name="create-payment"),
     path("confirm/", views.ConfirmPaymentView.as_view(), name="confirm-payment"),
+    path("webhook/", views.PaymentWebhookView.as_view(), name="payment-webhook"),  # 추가
 ]
