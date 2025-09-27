6f115285cba417174276809f17d2dc35983a8009
oscar2272 <oscar2272@naver.com>
2025-09-27 20:13:33 +0900

    fix locust file

--- drf_api/locustfile.py
+++ drf_api/locustfile.py
@@ -1,61 +1,46 @@
 import os
-import random
-from typing import Dict, List
-
 import requests
-from dotenv import load_dotenv
 from locust import HttpUser, between, task
-
-load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_ANON_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-USER_CREDENTIALS: List[Dict[str, str]] = []
-for i in range(1, 11):
-    USER_CREDENTIALS.append({"email": f"test{i}@naver.com", "password": "123456"})
-
+EMAIL = "test1@naver.com"
+PASSWORD = "123456"
 
 class NoteUser(HttpUser):
     wait_time = between(1, 3)
 
     def on_start(self):
-        if USER_CREDENTIALS:
-            self.user = random.choice(USER_CREDENTIALS)
-        else:
-            raise Exception("No test users available")
-
-        auth_response = requests.post(
+        # 로그인해서 토큰 받아오기
+        resp = requests.post(
             f"{SUPABASE_URL}/auth/v1/token?grant_type=password",
             headers={
                 "apikey": SUPABASE_ANON_KEY,
                 "Content-Type": "application/json",
             },
-            json={"email": self.user["email"], "password": self.user["password"]},
+            json={"email": EMAIL, "password": PASSWORD},
         )
+        if resp.status_code != 200:
+            raise Exception(f"Failed login: {resp.text}")
 
-        if auth_response.status_code == 200:
-            self.token = auth_response.json().get("access_token")
-            self.client.headers = {
-                "Authorization": f"Bearer {self.token}",
-                "apikey": SUPABASE_ANON_KEY,
-                "Content-Type": "application/json",
-            }
-        else:
-            raise Exception(
-                f"Failed to login as {self.user['email']} – {auth_response.text}"
-            )
+        access_token = resp.json()["access_token"]
 
-    @task(1)
+        # Locust 요청 헤더에 넣기
+        self.client.headers = {
+            "Authorization": f"Bearer {access_token}",
+            "apikey": SUPABASE_ANON_KEY,
+            "Content-Type": "application/json",
+        }
+
+    @task
     def explore_mypage(self):
         self.client.get("/api/friends/")
 
-    @task(1)
+    @task
     def get_received_requests(self):
-        """친구 요청 받은 목록 조회"""
         self.client.get("/api/friends/requests/received/")
 
-    @task(1)
+    @task
     def get_sent_requests(self):
-        """친구 요청 보낸 목록 조회"""
         self.client.get("/api/friends/requests/sent/")
